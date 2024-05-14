1dc45ccbc58b8e04155c59e076faa038826fc9ff
WEDDINGMADA <sariakandriat@gmail.com>
2024-05-14 21:16:29 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+let myImage = document.querySelector("img");
+myImage.addEventListener("click", function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/wedding.webp") {
+    myImage.setAttribute("src", "images/wedding2.webp");
+  } else {
+    myImage.setAttribute("src", "images/wedding.webp");
+  }
+});
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName () {
+  let myName = prompt("Veuillez saisir votre nom.");
+  localStorage.setItem("nom", myName);
+  myHeading.textContent = "My perfect wedding day, especially for you " + myName
+}
+if (!localStorage.getItem("nom")) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem("nom");
+  myHeading.textContent = "My perfect wedding day, especially for you " + storedName
+}
+myButton.addEventListener("click", function() {
+  setUserName();
+});
